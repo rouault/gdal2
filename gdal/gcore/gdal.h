65c551e18952b696495024aaaab28b03321ded91
Even Rouault <even.rouault@mines-paris.org>
2014-11-24 17:01:46 +0100

    Fix typo

--- gdal/gcore/gdal.h
+++ gdal/gcore/gdal.h
@@ -116,7 +116,7 @@ typedef enum
     /*! Average */                                      GRIORA_Average = 5,
     /*! Mode (selects the value which appears most often of all the sampled points) */
                                                         GRIORA_Mode = 6,
-    /*! Gauss bluring */                                GRIORA_Gauss = 7
+    /*! Gauss blurring */                               GRIORA_Gauss = 7
 } GDALRIOResampleAlg;
 
 /* NOTE to developers: only add members, and if so edit INIT_RASTERIO_EXTRA_ARG */
