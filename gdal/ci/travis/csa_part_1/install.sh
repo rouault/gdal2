9e8368113b4cb9b7e5e9ad3cc341523cf6e7d9cb
Even Rouault <even.rouault@spatialys.com>
2018-03-10 14:30:22 +0100

    WIP

---
+++ gdal/ci/travis/csa_part_1/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+. $(dirname $0)/../csa_common/install.sh
+
+cd gdal
+
+for dirname in port gcore frmts alg gnm; do
+    cd $dirname
+    scan-build -o scanbuildoutput -plist -v -enable-checker alpha.unix.cstring.OutOfBounds,alpha.unix.cstring.BufferOverlap make -j4
+    cd ..
+done
+
+cd ..
